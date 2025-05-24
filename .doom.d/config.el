ee8dd1e6dbd22b2ba7144d3e299fffbfebdf35fe
shrishjay <shrishjay2004@gmail.com>
2025-05-24 14:19:59 +0530

    modified doom's dashboard

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -75,6 +75,9 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;; Dashboard
+(setq fancy-splash-image (concat doom-private-dir "splash.png"))
+
 ;; Dired
 (use-package dired)
 ;; Company
